bd94780c36ecf3afa29d9077f16ee36584adc211
gambarotto <dgcdeveloper@gmail.com>
2022-04-11 15:57:21 -0300

    add home header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+import { Container, Header, HeaderContent, TotalCars } from './styles';
 
 const Home: React.FC = () => (
   <Container>
-    <Title>Home</Title>
+    <StatusBar
+      barStyle="light-content"
+      translucent
+      backgroundColor="transparent"
+    />
+    <Header>
+      <HeaderContent>
+        <Logo width={RFValue(108)} height={RFValue(12)} />
+        <TotalCars>Total de 12 carros</TotalCars>
+      </HeaderContent>
+    </Header>
   </Container>
 );
 
